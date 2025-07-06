3abf8ea929673e52f41eea067f8bed947bda3da2
Henry Hieu Nguyen <119148881+hieunguyen601@users.noreply.github.com>
2025-07-06 02:14:35 -0500

    constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,14 +1,9 @@
-const getBasePath = () => {
-  return typeof window !== 'undefined'
-    ? window.__NEXT_DATA__.assetPrefix || ''
-    : '';
-};
 
 export const projects = [
   {
     title: 'Nie Tattoo Studio Website',
     description: "A responsive website for a professional tattoo artist, built using HTML, CSS, Bootstrap, and JavaScript. The site includes an artist introduction, service details, and a contact form, all designed with a modern and clean layout for a smooth user experience across devices.",
-    image: `${getBasePath()}/images/1.PNG`,
+    image: `/images/1.PNG`,
     tags: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
     source: 'https://github.com/hieunguyen601/yen-nhi-tattoo-website',
     visit: 'https://www.yennhitattoo.com/',
@@ -17,7 +12,7 @@ export const projects = [
   {
     title: 'User Registration Form',
     description:"A responsive and modern user sign-up form created using HTML, CSS, and JavaScript. It includes fields for name, email, and password, with real-time input validation and user-friendly error messages. The form is styled for a clean, professional look and optimized for both desktop and mobile devices.",
-    image: `${getBasePath()}/images/2.PNG`,
+    image: `/images/2.PNG`,
     tags: ['HTML', 'CSS', 'JavaScript'],
     source: 'https://github.com/hieunguyen601/User-Sign-Up-Form-Component',
     visit: 'https://hieunguyen601.github.io/User-Sign-Up-Form-Component/',
@@ -26,7 +21,7 @@ export const projects = [
   {
     title: 'Age Calculator',
     description: "A responsive web application built with HTML, CSS, and JavaScript that allows users to input their birthdate and instantly calculate their age in years, months, and days. The app includes input validation to prevent invalid or future dates, and displays dynamic results with a clean and user-friendly interface.",
-    image: `${getBasePath()}/images/3.PNG`,
+    image: `/images/3.PNG`,
     tags: ['HTML', 'CSS', 'HTML'],
     source: 'https://github.com/hieunguyen601/Age-Calculation-App',
     visit: 'https://hieunguyen601.github.io/Age-Calculation-App/',
@@ -35,7 +30,7 @@ export const projects = [
   {
     title: 'To-Do List App',
     description: "A simple and interactive to-do list application built with HTML, CSS, and JavaScript. Users can add, delete, and mark tasks as completed. Tasks are managed dynamically using JavaScript, with a clean UI and responsive layout. This project demonstrates DOM manipulation, event handling, and basic front-end logic.",
-    image: `${getBasePath()}/images/4.PNG`,
+    image: `/images/4.PNG`,
     tags: ['HTML', 'CSS', 'JavaScript'],
     source: 'https://github.com/hieunguyen601/JS-to-do-list',
     visit: 'https://hieunguyen601.github.io/JS-to-do-list/',
